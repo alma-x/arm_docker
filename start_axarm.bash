395ac1e3c10032814315349cddaa7e41562992f5
gnoccoalpesto <74739268+gnoccoalpesto@users.noreply.github.com>
2022-06-05 23:46:37 +0200

    fixes

--- start_axarm.bash
+++ start_axarm.bash
@@ -3,19 +3,15 @@
 do_the_magic(){
 	
 	# update container tools
-	echo "update container"
 	/root/arm_docker/update_container.bash
 
 	# update alma-x ros repo
-	echo "update almax"
-	#/root/arm_docker/update_axarm.bash
+	/root/arm_docker/update_axarm.bash
 
 	# update erc ros repo
-	echo "update erc"
 	#/root/arm_docker/update_erc.bash
 
 	# build and setup
-	echo "build"
 	#/root/arm_docker/config_axarm.bash
 
 	# launch simulation
