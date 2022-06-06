3fb49e517292f37dfc3b90616e2b0831914730ac
gnoccoalpesto <74739268+gnoccoalpesto@users.noreply.github.com>
2022-06-06 11:12:47 +0200

    fixes

--- config_axarm.sh
+++ config_axarm.sh
@@ -3,17 +3,17 @@
 
 build_ros(){
 
-if [ -f /catkin_ws/devel/ ]; then
+if [ -d /catkin_ws/devel/ ]; then
 	rm -r /catkin_ws/devel/ 
 fi
 
-if [ -f /catkin_ws/build/ ]; then
+if [ -d /catkin_ws/build/ ]; then
 	rm -r /catkin_ws/build/ 
 fi
 
 #catkin build -qqqy
 cd /catkin_ws
-catkin_make -q
+catkin_make -q >/dev/null 2>&1
 }
 
 build_ros
