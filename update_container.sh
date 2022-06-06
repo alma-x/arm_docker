9cea6111d94eacfa678e5773334aa7543039d76d
gnoccoalpesto <74739268+gnoccoalpesto@users.noreply.github.com>
2022-06-06 11:06:27 +0200

    fixes

--- update_container.sh
+++ update_container.sh
@@ -10,14 +10,14 @@ cd /root/arm_docker
 update_git
 
 if [ -f /root/arm_docker/.bash_aliases ] ; then
-	if ! cmp /root/arm_docker/.bash_aliases root/.bash_aliases ; then
+	if ! cmp /root/arm_docker/.bash_aliases /root/.bash_aliases ; then
 		rm /root/.bash_aliases
 		cp /root/arm_docker/.bash_aliases /root
 	fi
 fi
 
 if [ -f /root/arm_docker/.bashrc ] ; then
-	if ! cmp /root/arm_docker/.bashrc root/.bashrc ; then
+	if ! cmp /root/arm_docker/.bashrc /root/.bashrc ; then
 		rm /root/.bashrc
 		cp /root/arm_docker/.bashrc /root && source /root/.bashrc
 	fi
